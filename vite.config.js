b65e576c33717c19bd15547a854dfe81ba33b153
Huong <huongnguyen.nth1709@gmail.com>
2025-01-22 14:43:48 +0700

    install required dependencies and add global css styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import babel from 'vite-plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      // Đảm bảo Babel sử dụng cấu hình từ file .babelrc
+      babelConfig: true, // Ensures it reads .babelrc or babel.config.js
+    }),
+  ],
+});
