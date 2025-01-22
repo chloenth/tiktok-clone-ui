cd700c76ee6aa3c7e3792708b174d238afd5b780
Huong <huongnguyen.nth1709@gmail.com>
2025-01-22 17:44:47 +0700

    add config router/layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Sidebar />
+        <div className='content'>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
---
+++ src/components/Layout/HeaderOnly/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import Header from './Header';
+
+function DefaultLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <div className='content'>{children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
