262fa3316d10906496e8b7e4058bb491bfa0ec7c
Huong <huongnguyen.nth1709@gmail.com>
2025-01-23 11:32:05 +0700

    build UI for search popper in header

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src='https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e18a0fb2e6dd7c2f62a9840652154e41~c5_1080x1080.jpeg?lk3s=a5d48078&nonce=17464&refresh_token=b2971667dca12b2a3a8c94be365528ac&x-expires=1737774000&x-signature=%2FdDzmsv0bXnwfph8ehkG2kFzCh0%3D&shp=a5d48078&shcp=81f88b70'
+        alt='Hoa'
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
