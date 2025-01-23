61d006a80fd3341f1123abc18860e327d76b96b0
Huong <huongnguyen.nth1709@gmail.com>
2025-01-23 20:37:26 +0700

    build UI for menu popper of more button

---
+++ src/components/Popper/Menu/MenuItem.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
+
+const cx = classNames.bind(styles);
+
+import Button from '~/components/Button';
+
+function MenuItem({ data }) {
+  return (
+    <div>
+      <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
+        {data.title}
+      </Button>
+    </div>
+  );
+}
+
+export default MenuItem;
