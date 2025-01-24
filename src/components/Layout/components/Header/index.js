71ff1649544851bab931aa0afee62cb9409a6531
Huong <huongnguyen.nth1709@gmail.com>
2025-01-24 10:25:25 +0700

    implement logic to handle sub menu in popper menu of more button

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -25,6 +25,19 @@ const MENU_ITEMS = [
   {
     icon: <FontAwesomeIcon icon={faEarthAsia} />,
     title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          code: 'en',
+          title: 'English',
+        },
+        {
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
   },
   {
     icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -45,6 +58,10 @@ function Header() {
       setSearchResult([]);
     }, 0);
   });
+
+  const handleMenuChange = (menuItem) => {
+    console.log(menuItem);
+  };
 
   return (
     <header className={cx('wrapper')}>
@@ -88,7 +105,7 @@ function Header() {
           <Button text>Upload</Button>
           <Button primary>Log in</Button>
 
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -6,10 +6,15 @@ const cx = classNames.bind(styles);
 
 import Button from '~/components/Button';
 
-function MenuItem({ data }) {
+function MenuItem({ data, onClick }) {
   return (
     <div>
-      <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
+      <Button
+        className={cx('menu-item')}
+        leftIcon={data.icon}
+        to={data.to}
+        onClick={onClick}
+      >
         {data.title}
       </Button>
     </div>
